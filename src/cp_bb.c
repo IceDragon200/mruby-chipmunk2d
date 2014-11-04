4fceaef3b4e5a22f7a73b070dcb1f6aefe948a81
Corey Powell <mistdragon100@gmail.com>
2014-11-04 13:39:28 -0500

    Completes Chipmunk2d::BB tests

--- src/cp_bb.c
+++ src/cp_bb.c
@@ -32,14 +32,12 @@ mrb_cp_bb_value(mrb_state *mrb, cpBB bb)
   mrb_value zero = mrb_float_value(mrb, 0.0);
   mrb_value argv[4] = { zero, zero, zero, zero };
   result = mrb_obj_new(mrb, mrb_cp_bb_class, 4, argv);
-
   result_bb = mrb_data_get_ptr(mrb, result, &mrb_cp_bb_type);
   *result_bb = bb;
-
   return result;
 }
 
-/*
+/* Chipmunk2d::BB#initialize(l, b, r, t)
  * @param [Float] l  left
  * @param [Float] b  bottom
  * @param [Float] r  right
@@ -54,20 +52,38 @@ bb_initialize(mrb_state *mrb, mrb_value self)
   mrb_float t;
   cpBB *bb;
   mrb_get_args(mrb, "ffff", &l, &b, &r, &t);
-
   bb = (cpBB*)DATA_PTR(self);
   if (bb) {
     mrb_cp_bb_free(mrb, bb);
   }
-
   bb = mrb_malloc(mrb, sizeof(cpBB));
   *bb = cpBBNew(l, b, r, t);
-
   mrb_data_init(self, bb, &mrb_cp_bb_type);
-
   return self;
 }
 
+/* Chipmunk2d::BB#initialize_copy(org)
+ * @param [Chipmunk2d::BB] org  original bb
+ */
+static mrb_value
+bb_initialize_copy(mrb_state *mrb, mrb_value self)
+{
+  cpBB *self_bb;
+  cpBB *org_bb;
+  mrb_get_args(mrb, "d", &org_bb, &mrb_cp_bb_type);
+  self_bb = (cpBB*)DATA_PTR(self);
+  if (self_bb) {
+    mrb_cp_bb_free(mrb, self_bb);
+  }
+  self_bb = mrb_malloc(mrb, sizeof(cpBB));
+  *self_bb = *org_bb;
+  mrb_data_init(self, self_bb, &mrb_cp_bb_type);
+  return self;
+}
+
+/* Chipmunk2d::BB#l
+ * @return [Float]
+ */
 static mrb_value
 bb_get_l(mrb_state *mrb, mrb_value self)
 {
@@ -76,6 +92,9 @@ bb_get_l(mrb_state *mrb, mrb_value self)
   return mrb_float_value(mrb, (mrb_float)bb->l);
 }
 
+/* Chipmunk2d::BB#l=(l)
+ * @param [Float] l
+ */
 static mrb_value
 bb_set_l(mrb_state *mrb, mrb_value self)
 {
@@ -87,6 +106,9 @@ bb_set_l(mrb_state *mrb, mrb_value self)
   return mrb_nil_value();
 }
 
+/* Chipmunk2d::BB#b
+ * @return [Float]
+ */
 static mrb_value
 bb_get_b(mrb_state *mrb, mrb_value self)
 {
@@ -95,6 +117,9 @@ bb_get_b(mrb_state *mrb, mrb_value self)
   return mrb_float_value(mrb, (mrb_float)bb->b);
 }
 
+/* Chipmunk2d::BB#b=(b)
+ * @param [Float] b
+ */
 static mrb_value
 bb_set_b(mrb_state *mrb, mrb_value self)
 {
@@ -106,6 +131,9 @@ bb_set_b(mrb_state *mrb, mrb_value self)
   return mrb_nil_value();
 }
 
+/* Chipmunk2d::BB#r
+ * @return [Float]
+ */
 static mrb_value
 bb_get_r(mrb_state *mrb, mrb_value self)
 {
@@ -114,6 +142,9 @@ bb_get_r(mrb_state *mrb, mrb_value self)
   return mrb_float_value(mrb, (mrb_float)bb->r);
 }
 
+/* Chipmunk2d::BB#r=(r)
+ * @param [Float] r
+ */
 static mrb_value
 bb_set_r(mrb_state *mrb, mrb_value self)
 {
@@ -125,6 +156,9 @@ bb_set_r(mrb_state *mrb, mrb_value self)
   return mrb_nil_value();
 }
 
+/* Chipmunk2d::BB#t
+ * @return [Float]
+ */
 static mrb_value
 bb_get_t(mrb_state *mrb, mrb_value self)
 {
@@ -133,6 +167,9 @@ bb_get_t(mrb_state *mrb, mrb_value self)
   return mrb_float_value(mrb, (mrb_float)bb->t);
 }
 
+/* Chipmunk2d::BB#t=(t)
+ * @param [Float] t
+ */
 static mrb_value
 bb_set_t(mrb_state *mrb, mrb_value self)
 {
@@ -144,48 +181,52 @@ bb_set_t(mrb_state *mrb, mrb_value self)
   return mrb_nil_value();
 }
 
+/* Chipmunk2d::BB#intersects?(other)
+ * @param [Chipmunk2d::BB] other
+ * @return [Boolean]
+ */
 static mrb_value
 bb_intersects(mrb_state *mrb, mrb_value self)
 {
   cpBB *bb;
   cpBB *bb2;
-  cpBool intersects;
   mrb_get_args(mrb, "d", &bb2, &mrb_cp_bb_type);
-
   bb = mrb_data_get_ptr(mrb, self, &mrb_cp_bb_type);
-
-  intersects = cpBBIntersects(*bb, *bb2);
-  return mrb_bool_value(intersects);
+  return mrb_bool_value(cpBBIntersects(*bb, *bb2));
 }
 
+/* Chipmunk2d::BB#contains_bb?(other)
+ * @param [Chipmunk2d::BB] other
+ * @return [Boolean]
+ */
 static mrb_value
 bb_contains_bb(mrb_state *mrb, mrb_value self)
 {
   cpBB *bb;
   cpBB *bb2;
-  cpBool contains_bb;
   mrb_get_args(mrb, "d", &bb2, &mrb_cp_bb_type);
-
   bb = mrb_data_get_ptr(mrb, self, &mrb_cp_bb_type);
-
-  contains_bb = cpBBContainsBB(*bb, *bb2);
-  return mrb_bool_value(contains_bb);
+  return mrb_bool_value(cpBBContainsBB(*bb, *bb2));
 }
 
+/* Chipmunk2d::BB#contains_vect?(vect)
+ * @param [Chipmunk2d::Vect] vect
+ * @return [Boolean]
+ */
 static mrb_value
 bb_contains_vect(mrb_state *mrb, mrb_value self)
 {
   cpBB *bb;
   cpVect *vect;
-  cpBool contains_vect;
   mrb_get_args(mrb, "d", &vect, &mrb_cp_vect_type);
-
   bb = mrb_data_get_ptr(mrb, self, &mrb_cp_bb_type);
-
-  contains_vect = cpBBContainsVect(*bb, *vect);
-  return mrb_bool_value(contains_vect);
+  return mrb_bool_value(cpBBContainsVect(*bb, *vect));
 }
 
+/* Chipmunk2d::BB#merge(other)
+ * @param [Chipmunk2d::BB] other
+ * @return [Chipmunk2d::BB]
+ */
 static mrb_value
 bb_merge(mrb_state *mrb, mrb_value self)
 {
@@ -196,16 +237,17 @@ bb_merge(mrb_state *mrb, mrb_value self)
   mrb_value zero = mrb_float_value(mrb, 0.0);
   mrb_value result_argv[4] = { zero, zero, zero, zero };
   mrb_get_args(mrb, "d", &bb2, &mrb_cp_bb_type);
-
   bb = mrb_data_get_ptr(mrb, self, &mrb_cp_bb_type);
-
   result = mrb_obj_new(mrb, mrb_cp_bb_class, 4, result_argv);
   bb_result = mrb_data_get_ptr(mrb, result, &mrb_cp_bb_type);
-
   *bb_result = cpBBMerge(*bb, *bb2);
   return result;
 }
 
+/* Chipmunk2d::BB#expand(other)
+ * @param [Chipmunk2d::Vect] other
+ * @return [Chipmunk2d::BB]
+ */
 static mrb_value
 bb_expand(mrb_state *mrb, mrb_value self)
 {
@@ -216,16 +258,16 @@ bb_expand(mrb_state *mrb, mrb_value self)
   mrb_value zero = mrb_float_value(mrb, 0.0);
   mrb_value result_argv[4] = { zero, zero, zero, zero };
   mrb_get_args(mrb, "d", &vect, &mrb_cp_vect_type);
-
   bb = mrb_data_get_ptr(mrb, self, &mrb_cp_bb_type);
-
   result = mrb_obj_new(mrb, mrb_cp_bb_class, 4, result_argv);
   bb_result = mrb_data_get_ptr(mrb, result, &mrb_cp_bb_type);
-
   *bb_result = cpBBExpand(*bb, *vect);
   return result;
 }
 
+/* Chipmunk2d::BB#center
+ * @return [Chipmunk2d::Vect]
+ */
 static mrb_value
 bb_center(mrb_state *mrb, mrb_value self)
 {
@@ -236,6 +278,9 @@ bb_center(mrb_state *mrb, mrb_value self)
   return mrb_cp_vect_value(mrb, center_vect);
 }
 
+/* Chipmunk2d::BB#area
+ * @return [Float]
+ */
 static mrb_value
 bb_area(mrb_state *mrb, mrb_value self)
 {
@@ -246,6 +291,11 @@ bb_area(mrb_state *mrb, mrb_value self)
   return mrb_float_value(mrb, area);
 }
 
+
+/* Chipmunk2d::BB#merged_area(other)
+ * @param [Chipmunk2d::BB] other
+ * @return [Float]
+ */
 static mrb_value
 bb_merged_area(mrb_state *mrb, mrb_value self)
 {
@@ -253,12 +303,16 @@ bb_merged_area(mrb_state *mrb, mrb_value self)
   cpBB *bb2;
   cpFloat merged_area;
   mrb_get_args(mrb, "d", &bb2, &mrb_cp_bb_type);
-
   bb = mrb_data_get_ptr(mrb, self, &mrb_cp_bb_type);
   merged_area = cpBBMergedArea(*bb, *bb2);
   return mrb_float_value(mrb, merged_area);
 }
 
+/* Chipmunk2d::BB#segment_query(v1, v2)
+ * @param [Chipmunk2d::Vect] v1
+ * @param [Chipmunk2d::Vect] v2
+ * @return [Float]
+ */
 static mrb_value
 bb_segment_query(mrb_state *mrb, mrb_value self)
 {
@@ -268,12 +322,16 @@ bb_segment_query(mrb_state *mrb, mrb_value self)
   cpFloat segment_query;
   mrb_get_args(mrb, "dd", &vect1, &mrb_cp_vect_type,
                           &vect2, &mrb_cp_vect_type);
-
   bb = mrb_data_get_ptr(mrb, self, &mrb_cp_bb_type);
   segment_query = cpBBSegmentQuery(*bb, *vect1, *vect2);
   return mrb_float_value(mrb, segment_query);
 }
 
+/* Chipmunk2d::BB#intersects_segment?(v1, v2)
+ * @param [Chipmunk2d::Vect] v1
+ * @param [Chipmunk2d::Vect] v2
+ * @return [Float]
+ */
 static mrb_value
 bb_intersects_segment(mrb_state *mrb, mrb_value self)
 {
@@ -283,12 +341,15 @@ bb_intersects_segment(mrb_state *mrb, mrb_value self)
   cpFloat intersects_segment;
   mrb_get_args(mrb, "dd", &vect1, &mrb_cp_vect_type,
                           &vect2, &mrb_cp_vect_type);
-
   bb = mrb_data_get_ptr(mrb, self, &mrb_cp_bb_type);
   intersects_segment = cpBBIntersectsSegment(*bb, *vect1, *vect2);
   return mrb_bool_value(intersects_segment);
 }
 
+/* Chipmunk2d::BB#clamp_vect(vect)
+ * @param [Chipmunk2d::Vect] vect
+ * @return [Chipmunk2d::Vect]
+ */
 static mrb_value
 bb_clamp_vect(mrb_state *mrb, mrb_value self)
 {
@@ -296,13 +357,15 @@ bb_clamp_vect(mrb_state *mrb, mrb_value self)
   cpVect *vect;
   cpVect clamp_vect;
   mrb_get_args(mrb, "d", &vect, &mrb_cp_vect_type);
-
   bb = mrb_data_get_ptr(mrb, self, &mrb_cp_bb_type);
-
   clamp_vect = cpBBClampVect(*bb, *vect);
   return mrb_cp_vect_value(mrb, clamp_vect);
 }
 
+/* Chipmunk2d::BB#wrap_vect(vect)
+ * @param [Chipmunk2d::Vect] vect
+ * @return [Chipmunk2d::Vect]
+ */
 static mrb_value
 bb_wrap_vect(mrb_state *mrb, mrb_value self)
 {
@@ -310,13 +373,15 @@ bb_wrap_vect(mrb_state *mrb, mrb_value self)
   cpVect *vect;
   cpVect wrap_vect;
   mrb_get_args(mrb, "d", &vect, &mrb_cp_vect_type);
-
   bb = mrb_data_get_ptr(mrb, self, &mrb_cp_bb_type);
-
   wrap_vect = cpBBWrapVect(*bb, *vect);
   return mrb_cp_vect_value(mrb, wrap_vect);
 }
 
+/* Chipmunk2d::BB#offset(vect)
+ * @param [Chipmunk2d::Vect] vect
+ * @return [Chipmunk2d::BB]
+ */
 static mrb_value
 bb_offset(mrb_state *mrb, mrb_value self)
 {
@@ -327,12 +392,9 @@ bb_offset(mrb_state *mrb, mrb_value self)
   mrb_value zero = mrb_float_value(mrb, 0.0);
   mrb_value result_argv[4] = { zero, zero, zero, zero };
   mrb_get_args(mrb, "d", &vect, &mrb_cp_vect_type);
-
   bb = mrb_data_get_ptr(mrb, self, &mrb_cp_bb_type);
-
   result = mrb_obj_new(mrb, mrb_cp_bb_class, 4, result_argv);
   bb_result = mrb_data_get_ptr(mrb, result, &mrb_cp_bb_type);
-
   *bb_result = cpBBOffset(*bb, *vect);
   return result;
 }
@@ -342,27 +404,28 @@ mrb_cp_bb_init(mrb_state *mrb, struct RClass *cp_module)
 {
   mrb_cp_bb_class = mrb_define_class_under(mrb, cp_module, "BB", mrb->object_class);
   MRB_SET_INSTANCE_TT(mrb_cp_bb_class, MRB_TT_DATA);
-
-  mrb_define_method(mrb, mrb_cp_bb_class, "initialize",         bb_initialize,         MRB_ARGS_REQ(4));
-  mrb_define_method(mrb, mrb_cp_bb_class, "l",                  bb_get_l,              MRB_ARGS_NONE());
-  mrb_define_method(mrb, mrb_cp_bb_class, "l=",                 bb_set_l,              MRB_ARGS_REQ(1));
-  mrb_define_method(mrb, mrb_cp_bb_class, "b",                  bb_get_b,              MRB_ARGS_NONE());
-  mrb_define_method(mrb, mrb_cp_bb_class, "b=",                 bb_set_b,              MRB_ARGS_REQ(1));
-  mrb_define_method(mrb, mrb_cp_bb_class, "r",                  bb_get_r,              MRB_ARGS_NONE());
-  mrb_define_method(mrb, mrb_cp_bb_class, "r=",                 bb_set_r,              MRB_ARGS_REQ(1));
-  mrb_define_method(mrb, mrb_cp_bb_class, "t",                  bb_get_t,              MRB_ARGS_NONE());
-  mrb_define_method(mrb, mrb_cp_bb_class, "t=",                 bb_set_t,              MRB_ARGS_REQ(1));
-  mrb_define_method(mrb, mrb_cp_bb_class, "intersects?",        bb_intersects,         MRB_ARGS_REQ(1));
-  mrb_define_method(mrb, mrb_cp_bb_class, "contains_bb?",       bb_contains_bb,        MRB_ARGS_REQ(1));
-  mrb_define_method(mrb, mrb_cp_bb_class, "contains_vect?",     bb_contains_vect,      MRB_ARGS_REQ(1));
-  mrb_define_method(mrb, mrb_cp_bb_class, "merge",              bb_merge,              MRB_ARGS_REQ(1));
-  mrb_define_method(mrb, mrb_cp_bb_class, "expand",             bb_expand,             MRB_ARGS_REQ(1));
-  mrb_define_method(mrb, mrb_cp_bb_class, "center",             bb_center,             MRB_ARGS_NONE());
-  mrb_define_method(mrb, mrb_cp_bb_class, "area",               bb_area,               MRB_ARGS_NONE());
-  mrb_define_method(mrb, mrb_cp_bb_class, "merged_area",        bb_merged_area,        MRB_ARGS_REQ(1));
-  mrb_define_method(mrb, mrb_cp_bb_class, "segment_query",      bb_segment_query,      MRB_ARGS_REQ(2));
-  mrb_define_method(mrb, mrb_cp_bb_class, "intersects_segment", bb_intersects_segment, MRB_ARGS_REQ(2));
-  mrb_define_method(mrb, mrb_cp_bb_class, "clamp_vect",         bb_clamp_vect,         MRB_ARGS_REQ(1));
-  mrb_define_method(mrb, mrb_cp_bb_class, "wrap_vect",          bb_wrap_vect,          MRB_ARGS_REQ(1));
-  mrb_define_method(mrb, mrb_cp_bb_class, "offset",             bb_offset,             MRB_ARGS_REQ(1));
+  /* */
+  mrb_define_method(mrb, mrb_cp_bb_class, "initialize",          bb_initialize,         MRB_ARGS_REQ(4));
+  mrb_define_method(mrb, mrb_cp_bb_class, "initialize_copy",     bb_initialize_copy,    MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, mrb_cp_bb_class, "l",                   bb_get_l,              MRB_ARGS_NONE());
+  mrb_define_method(mrb, mrb_cp_bb_class, "l=",                  bb_set_l,              MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, mrb_cp_bb_class, "b",                   bb_get_b,              MRB_ARGS_NONE());
+  mrb_define_method(mrb, mrb_cp_bb_class, "b=",                  bb_set_b,              MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, mrb_cp_bb_class, "r",                   bb_get_r,              MRB_ARGS_NONE());
+  mrb_define_method(mrb, mrb_cp_bb_class, "r=",                  bb_set_r,              MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, mrb_cp_bb_class, "t",                   bb_get_t,              MRB_ARGS_NONE());
+  mrb_define_method(mrb, mrb_cp_bb_class, "t=",                  bb_set_t,              MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, mrb_cp_bb_class, "intersects?",         bb_intersects,         MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, mrb_cp_bb_class, "contains_bb?",        bb_contains_bb,        MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, mrb_cp_bb_class, "contains_vect?",      bb_contains_vect,      MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, mrb_cp_bb_class, "merge",               bb_merge,              MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, mrb_cp_bb_class, "expand",              bb_expand,             MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, mrb_cp_bb_class, "center",              bb_center,             MRB_ARGS_NONE());
+  mrb_define_method(mrb, mrb_cp_bb_class, "area",                bb_area,               MRB_ARGS_NONE());
+  mrb_define_method(mrb, mrb_cp_bb_class, "merged_area",         bb_merged_area,        MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, mrb_cp_bb_class, "segment_query",       bb_segment_query,      MRB_ARGS_REQ(2));
+  mrb_define_method(mrb, mrb_cp_bb_class, "intersects_segment?", bb_intersects_segment, MRB_ARGS_REQ(2));
+  mrb_define_method(mrb, mrb_cp_bb_class, "clamp_vect",          bb_clamp_vect,         MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, mrb_cp_bb_class, "wrap_vect",           bb_wrap_vect,          MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, mrb_cp_bb_class, "offset",              bb_offset,             MRB_ARGS_REQ(1));
 }
--- test/bb.rb
+++ test/bb.rb
@@ -6,3 +6,158 @@ assert('Chipmunk2d::BB#initialize') do
   bb = Chipmunk2d::BB.new(1, 1, 1, 1)
   assert_kind_of(Chipmunk2d::BB, bb)
 end
+
+assert('Chipmunk2d::BB#initialize_copy') do
+  bb1 = Chipmunk2d::BB.new(1, 1, 1, 1)
+  bb2 = bb1.dup
+  assert_kind_of(Chipmunk2d::BB, bb2)
+  assert_false(bb2.equal?(bb1))
+end
+
+assert('Chipmunk2d::BB#l') do
+  bb = Chipmunk2d::BB.new(1, 1, 1, 1)
+  assert_equal(bb.l, 1)
+  bb.l = 3
+  assert_equal(bb.l, 3)
+  assert_equal(bb.b, 1)
+  assert_equal(bb.r, 1)
+  assert_equal(bb.t, 1)
+end
+
+assert('Chipmunk2d::BB#b') do
+  bb = Chipmunk2d::BB.new(1, 1, 1, 1)
+  assert_equal(bb.b, 1)
+  bb.b = 3
+  assert_equal(bb.l, 1)
+  assert_equal(bb.b, 3)
+  assert_equal(bb.r, 1)
+  assert_equal(bb.t, 1)
+end
+
+assert('Chipmunk2d::BB#r') do
+  bb = Chipmunk2d::BB.new(1, 1, 1, 1)
+  assert_equal(bb.r, 1)
+  bb.r = 3
+  assert_equal(bb.l, 1)
+  assert_equal(bb.b, 1)
+  assert_equal(bb.r, 3)
+  assert_equal(bb.t, 1)
+end
+
+assert('Chipmunk2d::BB#t') do
+  bb = Chipmunk2d::BB.new(1, 1, 1, 1)
+  assert_equal(bb.r, 1)
+  bb.t = 3
+  assert_equal(bb.l, 1)
+  assert_equal(bb.b, 1)
+  assert_equal(bb.r, 1)
+  assert_equal(bb.t, 3)
+end
+
+assert('Chipmunk2d::BB#intersects?') do
+  bb1 = Chipmunk2d::BB.new(0, 0, 10, 10)
+  bb2 = Chipmunk2d::BB.new(9, 9, 15, 15)
+  bb3 = Chipmunk2d::BB.new(19, 19, 10, 10)
+  assert_true(bb1.intersects?(bb2))
+  assert_false(bb1.intersects?(bb3))
+end
+
+assert('Chipmunk2d::BB#contains_bb?') do
+  bb1 = Chipmunk2d::BB.new(0, 0, 10, 10)
+  bb2 = Chipmunk2d::BB.new(9, 9, 10, 10)
+  bb3 = Chipmunk2d::BB.new(20, 20, 25, 25)
+  assert_true(bb1.contains_bb?(bb2))
+  assert_false(bb1.contains_bb?(bb3))
+end
+
+assert('Chipmunk2d::BB#merge') do
+  bb1 = Chipmunk2d::BB.new(0, 0, 10, 10)
+  bb2 = Chipmunk2d::BB.new(9, 9, 20, 13)
+  bb3 = bb1.merge(bb2)
+  assert_kind_of(Chipmunk2d::BB, bb3)
+  assert_equal(0, bb3.l)
+  assert_equal(0, bb3.b)
+  assert_equal(20, bb3.r)
+  assert_equal(13, bb3.t)
+end
+
+assert('Chipmunk2d::BB#expand') do
+  bb1 = Chipmunk2d::BB.new(0, 0, 10, 10)
+  vec = Chipmunk2d::Vect.new(20, 13)
+  bb3 = bb1.expand(vec)
+  assert_kind_of(Chipmunk2d::BB, bb3)
+  assert_equal(0, bb3.l)
+  assert_equal(0, bb3.b)
+  assert_equal(20, bb3.r)
+  assert_equal(13, bb3.t)
+end
+
+assert('Chipmunk2d::BB#center') do
+  bb = Chipmunk2d::BB.new(0, 0, 10, 10)
+  vec = bb.center
+  assert_kind_of(Chipmunk2d::Vect, vec)
+  assert_equal(5, vec.x)
+  assert_equal(5, vec.y)
+end
+
+assert('Chipmunk2d::BB#area') do
+  bb = Chipmunk2d::BB.new(0, 0, 10, 10)
+  area = bb.area
+  assert_kind_of(Float, area)
+  assert_equal(100, area)
+end
+
+assert('Chipmunk2d::BB#merged_area') do
+  bb1 = Chipmunk2d::BB.new(0, 0, 10, 10)
+  bb2 = Chipmunk2d::BB.new(3, 5, 15, 18)
+  area = bb1.merged_area(bb2)
+  assert_kind_of(Float, area)
+  assert_equal(270, area)
+end
+
+assert('Chipmunk2d::BB#segment_query') do
+  bb = Chipmunk2d::BB.new(0, 0, 10, 10)
+  v1 = Chipmunk2d::Vect.new(2, 5)
+  v2 = Chipmunk2d::Vect.new(3, 9)
+  result = bb.segment_query(v1, v2)
+  assert_kind_of(Float, result)
+end
+
+assert('Chipmunk2d::BB#intersects_segment?') do
+  bb = Chipmunk2d::BB.new(0, 0, 10, 10)
+  v1 = Chipmunk2d::Vect.new(2, 5)
+  v2 = Chipmunk2d::Vect.new(3, 11)
+  v3 = Chipmunk2d::Vect.new(11, 13)
+  v4 = Chipmunk2d::Vect.new(14, 14)
+  assert_true(bb.intersects_segment?(v1, v2))
+  assert_false(bb.intersects_segment?(v3, v4))
+end
+
+assert('Chipmunk2d::BB#clamp_vect') do
+  bb = Chipmunk2d::BB.new(0, 0, 10, 10)
+  vect = Chipmunk2d::Vect.new(-1, 11)
+  result = bb.clamp_vect(vect)
+  assert_kind_of(Chipmunk2d::Vect, result)
+  assert_equal(0, result.x)
+  assert_equal(10, result.y)
+end
+
+assert('Chipmunk2d::BB#wrap_vect') do
+  bb = Chipmunk2d::BB.new(0, 0, 10, 10)
+  vect = Chipmunk2d::Vect.new(-1, 11)
+  result = bb.wrap_vect(vect)
+  assert_kind_of(Chipmunk2d::Vect, result)
+  assert_equal(9, result.x)
+  assert_equal(1, result.y)
+end
+
+assert('Chipmunk2d::BB#offset') do
+  bb = Chipmunk2d::BB.new(0, 0, 10, 10)
+  vect = Chipmunk2d::Vect.new(-1, -5)
+  result = bb.offset(vect)
+  assert_kind_of(Chipmunk2d::BB, result)
+  assert_equal(-1, result.l)
+  assert_equal(-5, result.b)
+  assert_equal(9, result.r)
+  assert_equal(5, result.t)
+end
