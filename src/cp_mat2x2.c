8a599c2158db299970f71ed81f6bba24b0ba8a20
Corey Powell <mistdragon100@gmail.com>
2014-12-04 12:25:44 -0500

    Adds Chipmunk2d::Mat2x2 tests and documentation

--- src/cp_mat2x2.c
+++ src/cp_mat2x2.c
@@ -20,6 +20,14 @@ mrb_cp_mat2x2_free(mrb_state *mrb, void *ptr)
 
 struct mrb_data_type mrb_cp_mat2x2_type = { "Chipmunk2d::Mat2x2", mrb_cp_mat2x2_free };
 
+/*
+ * Chipmunk2d::Mat2x2#initialize(a, b, c, d)
+ * @param [Float] a
+ * @param [Float] b
+ * @param [Float] c
+ * @param [Float] d
+ * @return [self]
+ */
 static mrb_value
 mat2x2_initialize(mrb_state *mrb, mrb_value self)
 {
@@ -39,6 +47,10 @@ mat2x2_initialize(mrb_state *mrb, mrb_value self)
   return self;
 }
 
+/*
+ * Chipmunk2d::Mat2x2#a
+ * @return [Float]
+ */
 static mrb_value
 mat2x2_get_a(mrb_state *mrb, mrb_value self)
 {
@@ -47,6 +59,10 @@ mat2x2_get_a(mrb_state *mrb, mrb_value self)
   return mrb_float_value(mrb, (mrb_float)mat2x2->a);
 }
 
+/*
+ * Chipmunk2d::Mat2x2#a=
+ * @param [Float] a
+ */
 static mrb_value
 mat2x2_set_a(mrb_state *mrb, mrb_value self)
 {
@@ -58,6 +74,10 @@ mat2x2_set_a(mrb_state *mrb, mrb_value self)
   return mrb_nil_value();
 }
 
+/*
+ * Chipmunk2d::Mat2x2#b
+ * @return [Float]
+ */
 static mrb_value
 mat2x2_get_b(mrb_state *mrb, mrb_value self)
 {
@@ -66,6 +86,10 @@ mat2x2_get_b(mrb_state *mrb, mrb_value self)
   return mrb_float_value(mrb, (mrb_float)mat2x2->b);
 }
 
+/*
+ * Chipmunk2d::Mat2x2#b=
+ * @param [Float] b
+ */
 static mrb_value
 mat2x2_set_b(mrb_state *mrb, mrb_value self)
 {
@@ -77,6 +101,10 @@ mat2x2_set_b(mrb_state *mrb, mrb_value self)
   return mrb_nil_value();
 }
 
+/*
+ * Chipmunk2d::Mat2x2#c
+ * @return [Float]
+ */
 static mrb_value
 mat2x2_get_c(mrb_state *mrb, mrb_value self)
 {
@@ -85,6 +113,10 @@ mat2x2_get_c(mrb_state *mrb, mrb_value self)
   return mrb_float_value(mrb, (mrb_float)mat2x2->c);
 }
 
+/*
+ * Chipmunk2d::Mat2x2#c=
+ * @param [Float] c
+ */
 static mrb_value
 mat2x2_set_c(mrb_state *mrb, mrb_value self)
 {
@@ -96,6 +128,10 @@ mat2x2_set_c(mrb_state *mrb, mrb_value self)
   return mrb_nil_value();
 }
 
+/*
+ * Chipmunk2d::Mat2x2#d
+ * @return [Float]
+ */
 static mrb_value
 mat2x2_get_d(mrb_state *mrb, mrb_value self)
 {
@@ -104,6 +140,10 @@ mat2x2_get_d(mrb_state *mrb, mrb_value self)
   return mrb_float_value(mrb, (mrb_float)mat2x2->d);
 }
 
+/*
+ * Chipmunk2d::Mat2x2#d=
+ * @param [Float] d
+ */
 static mrb_value
 mat2x2_set_d(mrb_state *mrb, mrb_value self)
 {
@@ -115,6 +155,10 @@ mat2x2_set_d(mrb_state *mrb, mrb_value self)
   return mrb_nil_value();
 }
 
+/*
+ * Chipmunk2d::Mat2x2#transform(vect)
+ * @param [Chipmunk2d::Vect] vect
+ */
 static mrb_value
 mat2x2_transform(mrb_state *mrb, mrb_value self)
 {
@@ -131,14 +175,14 @@ mrb_cp_mat2x2_init(mrb_state *mrb, struct RClass *cp_module)
   mrb_cp_mat2x2_class = mrb_define_class_under(mrb, cp_module, "Mat2x2", mrb->object_class);
   MRB_SET_INSTANCE_TT(mrb_cp_mat2x2_class, MRB_TT_DATA);
   /* */
-  mrb_define_method(mrb, mrb_cp_mat2x2_class, "initialize",         mat2x2_initialize,         MRB_ARGS_REQ(4));
-  mrb_define_method(mrb, mrb_cp_mat2x2_class, "a",                  mat2x2_get_a,              MRB_ARGS_NONE());
-  mrb_define_method(mrb, mrb_cp_mat2x2_class, "a=",                 mat2x2_set_a,              MRB_ARGS_REQ(1));
-  mrb_define_method(mrb, mrb_cp_mat2x2_class, "b",                  mat2x2_get_b,              MRB_ARGS_NONE());
-  mrb_define_method(mrb, mrb_cp_mat2x2_class, "b=",                 mat2x2_set_b,              MRB_ARGS_REQ(1));
-  mrb_define_method(mrb, mrb_cp_mat2x2_class, "c",                  mat2x2_get_c,              MRB_ARGS_NONE());
-  mrb_define_method(mrb, mrb_cp_mat2x2_class, "c=",                 mat2x2_set_c,              MRB_ARGS_REQ(1));
-  mrb_define_method(mrb, mrb_cp_mat2x2_class, "d",                  mat2x2_get_d,              MRB_ARGS_NONE());
-  mrb_define_method(mrb, mrb_cp_mat2x2_class, "d=",                 mat2x2_set_d,              MRB_ARGS_REQ(1));
-  mrb_define_method(mrb, mrb_cp_mat2x2_class, "transform",          mat2x2_transform,          MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, mrb_cp_mat2x2_class, "initialize", mat2x2_initialize, MRB_ARGS_REQ(4));
+  mrb_define_method(mrb, mrb_cp_mat2x2_class, "a",          mat2x2_get_a,      MRB_ARGS_NONE());
+  mrb_define_method(mrb, mrb_cp_mat2x2_class, "a=",         mat2x2_set_a,      MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, mrb_cp_mat2x2_class, "b",          mat2x2_get_b,      MRB_ARGS_NONE());
+  mrb_define_method(mrb, mrb_cp_mat2x2_class, "b=",         mat2x2_set_b,      MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, mrb_cp_mat2x2_class, "c",          mat2x2_get_c,      MRB_ARGS_NONE());
+  mrb_define_method(mrb, mrb_cp_mat2x2_class, "c=",         mat2x2_set_c,      MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, mrb_cp_mat2x2_class, "d",          mat2x2_get_d,      MRB_ARGS_NONE());
+  mrb_define_method(mrb, mrb_cp_mat2x2_class, "d=",         mat2x2_set_d,      MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, mrb_cp_mat2x2_class, "transform",  mat2x2_transform,  MRB_ARGS_REQ(1));
 }
--- test/mat2x2.rb
+++ test/mat2x2.rb
@@ -1,3 +1,70 @@
 assert('Chipmunk2d::Mat2x2 type') do
   assert_kind_of(Class, Chipmunk2d::Mat2x2)
 end
+
+assert('Chipmunk2d::Mat2x2#initialize') do
+  mat = Chipmunk2d::Mat2x2.new(1.0, 2.0, 3.0, 4.0)
+  assert_kind_of(Chipmunk2d::Mat2x2, mat)
+  assert_kind_of(Float, mat.a)
+  assert_kind_of(Float, mat.b)
+  assert_kind_of(Float, mat.c)
+  assert_kind_of(Float, mat.d)
+  assert_float(1.0, mat.a)
+  assert_float(2.0, mat.b)
+  assert_float(3.0, mat.c)
+  assert_float(4.0, mat.d)
+end
+
+# attribute tests, make sure that changing one doesn't affect the others.
+assert('Chipmunk2d::Mat2x2#a') do
+  mat = Chipmunk2d::Mat2x2.new(1.0, 2.0, 3.0, 4.0)
+  assert_float(1.0, mat.a)
+
+  mat.a = 5.0
+  assert_float(5.0, mat.a)
+  assert_float(2.0, mat.b)
+  assert_float(3.0, mat.c)
+  assert_float(4.0, mat.d)
+end
+
+assert('Chipmunk2d::Mat2x2#b') do
+  mat = Chipmunk2d::Mat2x2.new(1.0, 2.0, 3.0, 4.0)
+  assert_float(2.0, mat.b)
+
+  mat.b = 5.0
+  assert_float(1.0, mat.a)
+  assert_float(5.0, mat.b)
+  assert_float(3.0, mat.c)
+  assert_float(4.0, mat.d)
+end
+
+assert('Chipmunk2d::Mat2x2#c') do
+  mat = Chipmunk2d::Mat2x2.new(1.0, 2.0, 3.0, 4.0)
+  assert_float(3.0, mat.c)
+
+  mat.c = 5.0
+  assert_float(1.0, mat.a)
+  assert_float(2.0, mat.b)
+  assert_float(5.0, mat.c)
+  assert_float(4.0, mat.d)
+end
+
+assert('Chipmunk2d::Mat2x2#d') do
+  mat = Chipmunk2d::Mat2x2.new(1.0, 2.0, 3.0, 4.0)
+  assert_float(4.0, mat.d)
+
+  mat.d = 5.0
+  assert_float(1.0, mat.a)
+  assert_float(2.0, mat.b)
+  assert_float(3.0, mat.c)
+  assert_float(5.0, mat.d)
+end
+
+assert('Chipmunk2d::Mat2x2#transform') do
+  mat = Chipmunk2d::Mat2x2.new(1.0, 1.0, 3.0, 3.0)
+  vect = Chipmunk2d::Vect.new(4, 2)
+  result = mat.transform(vect)
+  assert_kind_of(Chipmunk2d::Vect, result)
+  assert_float(6, result.x)
+  assert_float(18, result.y)
+end
