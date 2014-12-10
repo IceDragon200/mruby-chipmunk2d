133cef040729ff299b735b6f631c52f6d6ccd431
Corey Powell <mistdragon100@gmail.com>
2014-12-10 11:32:04 -0500

    Tests Chipmunk2d::PivotJoint

--- test/pivot_joint.rb
+++ test/pivot_joint.rb
@@ -1,3 +1,17 @@
+def assert_vect_equal(a, b)
+  msg = 'Expected to be equal' unless msg
+  diff = assertion_diff(a, b)
+  assert_true(a.fuzzy_eql?(b), msg, diff)
+end
+
+gen = lambda do
+  a = Chipmunk2d::Body.new(100, 1)
+  b = Chipmunk2d::Body.new(100, 1)
+  anchor_a = Chipmunk2d::Vect.new(1, 0)
+  anchor_b = Chipmunk2d::Vect.new(-1, 1)
+  joint = Chipmunk2d::PivotJoint.new(a, b, anchor_a, anchor_b)
+end
+
 assert('Chipmunk2d::PivotJoint type') do
   assert_kind_of(Class, Chipmunk2d::PivotJoint)
 end
@@ -14,4 +28,20 @@ assert('Chipmunk2d::PivotJoint#initialize') do
   assert_equal(b, joint.body_b)
   assert_equal(anchor_a, joint.anchor_a)
   assert_equal(anchor_b, joint.anchor_b)
+end
+
+assert('Chipmunk2d::PivotJoint#anchor_a') do
+  c = gen.call
+  assert_kind_of(Chipmunk2d::Vect, c.anchor_a)
+  v = Chipmunk2d::Vect.new(2, 3)
+  c.anchor_a = v
+  assert_equal(v, c.anchor_a)
+end
+
+assert('Chipmunk2d::PivotJoint#anchor_b') do
+  c = gen.call
+  assert_kind_of(Chipmunk2d::Vect, c.anchor_b)
+  v = Chipmunk2d::Vect.new(2, 3)
+  c.anchor_b = v
+  assert_equal(v, c.anchor_b)
 end
