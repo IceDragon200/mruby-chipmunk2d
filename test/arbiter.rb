3840f7577497bc557aafcbcb699707a8783ea03e
Corey Powell <mistdragon100@gmail.com>
2014-12-09 23:20:35 -0500

    Stubs Chipmunk2d::Arbiter tests

--- test/arbiter.rb
+++ test/arbiter.rb
@@ -1,3 +1,124 @@
 assert('Chipmunk2d::Arbiter type') do
   assert_kind_of(Class, Chipmunk2d::Arbiter)
 end
+
+assert('Chipmunk2d::Arbiter#restitution') do
+  true
+end
+
+assert('Chipmunk2d::Arbiter#friction') do
+  true
+end
+
+assert('Chipmunk2d::Arbiter#surface_velocity') do
+  true
+end
+
+assert('Chipmunk2d::Arbiter#total_impulse') do
+  true
+end
+
+assert('Chipmunk2d::Arbiter#total_ke') do
+  true
+end
+
+assert('Chipmunk2d::Arbiter#ignore') do
+  true
+end
+
+assert('Chipmunk2d::Arbiter#shapes') do
+  true
+end
+
+assert('Chipmunk2d::Arbiter#bodies') do
+  true
+end
+
+assert('Chipmunk2d::Arbiter#contact_point_set') do
+  true
+end
+
+assert('Chipmunk2d::Arbiter#first_contact') do
+  true
+end
+
+assert('Chipmunk2d::Arbiter#removal') do
+  true
+end
+
+assert('Chipmunk2d::Arbiter#normal') do
+  true
+end
+
+assert('Chipmunk2d::Arbiter#point_a') do
+  true
+end
+
+assert('Chipmunk2d::Arbiter#point_b') do
+  true
+end
+
+assert('Chipmunk2d::Arbiter#depth') do
+  true
+end
+
+assert('Chipmunk2d::Arbiter#call_wildcard_begin_a') do
+  true
+end
+
+assert('Chipmunk2d::Arbiter#call_wildcard_begin_b') do
+  true
+end
+
+assert('Chipmunk2d::Arbiter#call_wildcard_pre_solve_a') do
+  true
+end
+
+assert('Chipmunk2d::Arbiter#call_wildcard_pre_solve_b') do
+  true
+end
+
+assert('Chipmunk2d::Arbiter#call_wildcard_post_solve_a') do
+  true
+end
+
+assert('Chipmunk2d::Arbiter#call_wildcard_post_solve_b') do
+  true
+end
+
+assert('Chipmunk2d::Arbiter#call_wildcard_separate_a') do
+  true
+end
+
+assert('Chipmunk2d::Arbiter#call_wildcard_separate_b') do
+  true
+end
+
+__END__
+restitution
+restitution=
+friction
+friction=
+surface_velocity
+surface_velocity=
+total_impulse
+total_ke
+ignore
+shapes
+bodies
+contact_point_set
+contact_point_set=
+first_contact
+removal
+normal
+point_a
+point_b
+depth
+call_wildcard_begin_a
+call_wildcard_begin_b
+call_wildcard_pre_solve_a
+call_wildcard_pre_solve_b
+call_wildcard_post_solve_a
+call_wildcard_post_solve_b
+call_wildcard_separate_a
+call_wildcard_separate_b
