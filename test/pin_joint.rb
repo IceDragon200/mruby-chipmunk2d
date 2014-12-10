4955908b8de0d37f415acb8e47284ad5ca3ead39
Corey Powell <mistdragon100@gmail.com>
2014-12-09 23:20:04 -0500

    Completes Chipmunk2d::PinJoint test

--- test/pin_joint.rb
+++ test/pin_joint.rb
@@ -1,3 +1,17 @@
+def assert_vect_equal(a, b)
+  msg = 'Expected to be equal' unless msg
+  diff = assertion_diff(a, b)
+  assert_true(a.fuzzy_eql?(b), msg, diff)
+end
+
+gen = lambda do
+  a = Chipmunk2d::Body.new(100, 1)
+  b = Chipmunk2d::Body.new(100, 1)
+  anchor_a = Chipmunk2d::Vect.new(1, 0)
+  anchor_b = Chipmunk2d::Vect.new(-1, 1)
+  Chipmunk2d::PinJoint.new(a, b, anchor_a, anchor_b)
+end
+
 assert('Chipmunk2d::PinJoint type') do
   assert_kind_of(Class, Chipmunk2d::PinJoint)
 end
@@ -14,4 +28,30 @@ assert('Chipmunk2d::PinJoint#initialize') do
   assert_equal(b, joint.body_b)
   assert_equal(anchor_a, joint.anchor_a)
   assert_equal(anchor_b, joint.anchor_b)
+end
+
+
+assert('Chipmunk2d::PinJoint#anchor_a') do
+  c = gen.call
+  assert_kind_of(Chipmunk2d::Vect, c.anchor_a)
+  v = Chipmunk2d::Vect.new(12, 8)
+  c.anchor_a = v
+  assert_vect_equal(v, c.anchor_a)
+end
+
+assert('Chipmunk2d::PinJoint#anchor_b') do
+  c = gen.call
+  assert_kind_of(Chipmunk2d::Vect, c.anchor_b)
+  v = Chipmunk2d::Vect.new(12, 8)
+  c.anchor_b = v
+  assert_vect_equal(v, c.anchor_b)
+end
+
+assert('Chipmunk2d::PinJoint#dist') do
+  c = gen.call
+  assert_kind_of(Float, c.dist)
+  c.dist = 0.7
+  assert_float(0.7, c.dist)
+  c.dist = 1.7
+  assert_float(1.7, c.dist)
 end
